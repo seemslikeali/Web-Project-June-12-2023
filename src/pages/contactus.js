52628b4d0d3f64c352a90ff1624952c4708957d3
Ali Shakeel <seems.like.ali@gmail.com>
2023-07-05 08:40:23 -0600

    Form now function. Using Axios and formspree.

--- src/pages/contactus.js
+++ src/pages/contactus.js
@@ -3,7 +3,8 @@ import Layout from '@/components/Layout'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
+import axios from 'axios';
 import { motion, useMotionValue, onMouseMove, onMouseLeave } from 'framer-motion'
 
 // images
@@ -11,11 +12,71 @@ import { motion, useMotionValue, onMouseMove, onMouseLeave } from 'framer-motion
 import TransitionEffect from '@/components/TransitionEffect'
 
 const contactus = () => {
+
+    const [status, setStatus] = useState({
+        submitted: false,
+        submitting: false,
+        info: { error: false, msg: null },
+    });
+    const [inputs, setInputs] = useState({
+        email: '',
+        message: '',
+    });
+    const handleServerResponse = (ok, msg) => {
+        if (ok) {
+            setStatus({
+                submitted: true,
+                submitting: false,
+                info: { error: false, msg: msg },
+            });
+            setInputs({
+                name: '',
+                email: '',
+                message: '',
+            });
+        } else {
+            setStatus({
+                info: { error: true, msg: msg },
+            });
+        }
+    };
+    const handleOnChange = (e) => {
+        e.persist();
+        setInputs((prev) => ({
+            ...prev,
+            [e.target.id]: e.target.value,
+        }));
+        setStatus({
+            submitted: false,
+            submitting: false,
+            info: { error: false, msg: null },
+        });
+    };
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        setStatus((prevStatus) => ({ ...prevStatus, submitting: true }));
+        axios({
+            method: 'POST',
+            url: 'https://formspree.io/f/xwkdkkey',
+            data: inputs,
+        })
+            .then((response) => {
+                handleServerResponse(
+                    true,
+                    'Thank you, your message has been submitted.',
+                );
+            })
+            .catch((error) => {
+                handleServerResponse(false, error.response.data.error);
+            });
+    };
+
     return (
         <>
             <Head>
                 <title>Ali Shakeel | Contact Page </title>
                 <meta name='description' content='about us' />
+
             </Head>
             <TransitionEffect />
             <main className='w-full mb-16 flex flex-col items-center justify-center overflow-hidden dark:text-light'>
@@ -67,21 +128,39 @@ const contactus = () => {
                                         </div>
                                     </div>
                                     <div class="mx-auto max-w-[700px]">
-                                        <form>
+                                        <form onSubmit={handleOnSubmit}>
                                             <div class="relative mb-6" data-te-input-wrapper-init>
-                                                <input type="text"
+                                                <input
                                                     class="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                                                    id="exampleInput90" placeholder="Name" />
-                                                <label
+                                                    placeholder="Name"
+
+                                                    id="name"
+                                                    type="name"
+                                                    name="name"
+                                                    onChange={handleOnChange}
+                                                    required
+                                                    value={inputs.name}
+
+                                                />
+                                                <label htmlFor='name'
                                                     class="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
                                                     for="exampleInput90">Name
                                                 </label>
                                             </div>
                                             <div class="relative mb-6" data-te-input-wrapper-init>
-                                                <input type="email"
+                                                <input
                                                     class="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                                                    id="exampleInput91" placeholder="Email address" />
-                                                <label
+                                                    placeholder="Email address"
+
+                                                    id="email"
+                                                    type="email"
+                                                    name="_replyto"
+                                                    onChange={handleOnChange}
+                                                    required
+                                                    value={inputs.email}
+
+                                                />
+                                                <label htmlFor="email"
                                                     class="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
                                                     for="exampleInput91">Email address
                                                 </label>
@@ -89,16 +168,33 @@ const contactus = () => {
                                             <div class="relative mb-6" data-te-input-wrapper-init>
                                                 <textarea
                                                     class="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                                                    id="exampleFormControlTextarea1" rows="3" placeholder="Your message"></textarea>
-                                                <label for="exampleFormControlTextarea1"
+                                                    rows="3" placeholder="Your message"
+
+                                                    id="message"
+                                                    name="message"
+                                                    onChange={handleOnChange}
+                                                    required
+                                                    value={inputs.message}
+
+                                                ></textarea>
+                                                <label htmlFor="message"
                                                     class="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">Message</label>
                                             </div>
 
-                                            <button type="button" data-te-ripple-init data-te-ripple-color="light"
+                                            <button type="submit" disabled={status.submitting} data-te-ripple-init data-te-ripple-color="light"
                                                 class="inline-block w-full rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] lgl:mb-0">
-                                                Send
+                                                {!status.submitting
+                                                    ? !status.submitted
+                                                        ? 'Submit'
+                                                        : 'Submitted'
+                                                    : 'Submitting...'}
+
                                             </button>
                                         </form>
+                                        {status.info.error && (
+                                            <div className="error">Error: {status.info.msg}</div>
+                                        )}
+                                        {!status.info.error && status.info.msg && <p>{status.info.msg}</p>}
                                         <p className='text-center pt-8'>If you don't hear from us within 2 business days please call us!</p>
                                     </div>
                                 </div>
