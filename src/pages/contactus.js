d330eb4d7f6180c2f16c598f6be820a395c53933
ginsan11 <thezerodude2@gmail.com>
2023-07-20 08:45:57 -0600

    index contactus
    removed more stuff

--- src/pages/contactus.js
+++ src/pages/contactus.js
@@ -83,8 +83,6 @@ const ContactUs = () => {
                 <meta name="robots" content="index, follow" />
                 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
                 <meta name="language" content="English" />
-                <html lang="en" />
-                <link rel="canonical" href="https://www.prairievistas.ca" />
                 <meta name="author" content="Prairie Vistas" />
 
                 {/*Social media meta*/} 
